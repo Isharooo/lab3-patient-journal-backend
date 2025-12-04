ccc498cbb30ea1ca295835bc46d0e5e4b143cbb1
Isharooo <isharzeito@gmail.com>
2025-12-05 00:57:08 +0100

    small fix

--- src/test/java/se/kth/lab3/patient_journal_backend_microservices/PatientJournalBackendMicroservicesApplicationTests.java
+++ src/test/java/se/kth/lab3/patient_journal_backend_microservices/PatientJournalBackendMicroservicesApplicationTests.java
@@ -7,15 +7,17 @@ import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import se.kth.lab3.patient_journal_backend_microservices.dto.JournalEntryDTO;
+import se.kth.lab3.patient_journal_backend_microservices.dto.PatientCommandDTO;
 import se.kth.lab3.patient_journal_backend_microservices.dto.PatientDTO;
 
 @SpringBootTest(properties = {
         "kafka.topic.patient=test-patient-topic",
         "kafka.topic.journal=test-journal-topic",
-        "spring.kafka.bootstrap-servers=localhost:9092"
+        "kafka.topic.patient-commands=test-patient-commands",
+        "spring.kafka.bootstrap-servers=localhost:9092",
+        "spring.kafka.consumer.group-id=test-group"
 })
 @ActiveProfiles("test")
-// OBS: Vi har tagit bort @Import(TestSecurityConfig.class) för att slippa krocken!
 class PatientJournalBackendMicroservicesApplicationTests {
 
     // Mocka Kafka så att appen startar utan Kafka-server
@@ -24,6 +26,9 @@ class PatientJournalBackendMicroservicesApplicationTests {
 
     @MockitoBean
     private KafkaTemplate<String, JournalEntryDTO> journalKafkaTemplate;
+
+    @MockitoBean
+    private KafkaTemplate<String, PatientCommandDTO> commandKafkaTemplate;
 
     // Mocka JwtDecoder så att SecurityConfig startar utan Keycloak
     @MockitoBean
